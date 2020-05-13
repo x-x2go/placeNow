28328e47a36298b0e427cc91f97bbc59e6ca9666
x-x2go <xxxvi_@naver.com>
2020-05-13 23:28:53 +0900

    ✨search bar (tool)

---
+++ static/assets/search.js
@@ -0,0 +1,30 @@
+const searchCafe = document.getElementById("cafe");
+const searchRestaurants = document.getElementById("restaurants");
+const searchMarket = document.getElementById("market");
+const searchHospital = document.getElementById("hospital");
+
+
+function showCafe(){
+
+}
+
+function showRestaurant(){
+
+}
+
+function showMarket(){
+
+}
+
+function showHospital(){
+    
+}
+
+function init(){
+    searchCafe.addEventListener("click", showCafe);
+    searchRestaurants.addEventListener("click", showRestaurant);
+    searchMarket.addEventListener("click", showMarket);
+    searchHospital.addEventListener("click", showHospital);
+}
+
+init();
